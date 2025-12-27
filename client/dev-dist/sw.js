82a4585ead0b70722c01573a18a660b5a0d26c89
yile <wannengxin@gmail.com>
2025-12-27 09:10:36 +0800

    stage

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.cu8p9t8f44o"
+    "revision": "0.82halgnlqj"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
